68c16370a272b3d2313467cf75d1f1f5cd07d4a1
Marid <marid@marid.Dlink>
2023-07-17 23:57:40 +0300

    Deploy

---
+++ backend/api/management/commands/import_csv.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.core.management import BaseCommand
+
+from .load_csv_data import Command as sqlite
+from .load_data import Command as postgres
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if settings.DATABASES['default']['ENGINE'] == 'django.db.backends.sqlite3':
+            sqlite().handle(self, *args, **options)
+        elif settings.DATABASES['default']['ENGINE'] == 'django.db.backends.postgresql':
+            postgres.handle(self, *args, **options)
+        else:
+            raise Exception('Unknown database')
---
+++ backend/api/management/commands/load_data.py
@@ -0,0 +1,57 @@
+from sqlalchemy import create_engine
+
+from django.conf import settings
+from django.core.management import BaseCommand
+
+from recipes.models import MeasurementUnit
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        try:
+            import pandas as pd
+        except Exception as e:
+            print('Unable to import pandas', e)
+
+        user = settings.DATABASES['default']['USER']
+        password = settings.DATABASES['default']['PASSWORD']
+        database_name = settings.DATABASES['default']['NAME']
+        host = settings.DATABASES['default']['HOST']
+        port = settings.DATABASES['default']['PORT']
+        url_struct = 'postgresql://{user}:{password}@{host}:{port}/{database_name}'
+        database_url = url_struct.format(user=user,
+                                         password=password,
+                                         host=host,
+                                         port=port,
+                                         database_name=database_name,)
+
+        engine = create_engine(database_url, echo=False)
+
+        self.handle_1(engine, pd)
+
+    def handle_1(self, engine, pd):
+        try:
+            df = pd.read_csv(
+                '/home/marid/Dev/foodgram-project-react/data/ingredients1.csv',
+                usecols=[1]
+            )
+            df.drop_duplicates(keep='first', inplace=True)
+            df = df.assign(counted=True)
+            df.to_sql('recipes_measurementunit',
+                      engine, if_exists='append',
+                      index=False)
+        except Exception as e:
+            print('units', e)
+        try:
+            qs = MeasurementUnit.objects.values_list('id', 'unit')
+            res = dict((v, k) for k, v in dict(qs).items())
+            df = pd.read_csv(
+                '/home/marid/Dev/foodgram-project-react/data/ingredients1.csv'
+            )
+            df['measurement_unit_id'] = df['unit'].map(res)
+            df = df.drop('unit', axis=1)
+            df.to_sql('recipes_ingredient',
+                      engine, if_exists='append',
+                      index=False)
+        except Exception as e:
+            print('ingredients', e)
